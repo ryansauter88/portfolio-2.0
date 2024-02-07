ad2ac112d09dbd25f12779a4af8570f62ebe8487
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2024-02-06 23:37:33 -0500

    forgot to update ONE LINK

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return(
         <>
             <h5>Thanks for your interest! Here are clickable buttons to my profiles on other platforms.</h5>
-            <a href="https://github.com/RedKnight88"><img id="github" className="icon" src={GitHubLogo}></img></a>
+            <a href="https://github.com/ryansauter88"><img id="github" className="icon" src={GitHubLogo}></img></a>
             <a href="https://www.linkedin.com/in/ryansauter88/"><img id="linkedin" className="icon" src={LinkedInLogo}></img></a>
         </>
     );
