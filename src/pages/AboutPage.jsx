fff49c73cf3d8f3f792422899f83bdf29b4801f7
Ryan Sauter <127162163+ryansauter88@users.noreply.github.com>
2024-05-06 13:47:34 -0400

    Update AboutPage.jsx
    i'm a workin man now

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,8 +6,8 @@ function AboutPage() {
         <>
             <h1>About Me</h1>
             <img className="headshot" src={HeaderImage}/>
-            <p>My name is Ryan Sauter, and I am an aspiring Software Developer. Since my time at the prestigious Purdue University, I have engaged with different Computer Science classes and the basics of coding. Starting with C and MATLAB, I have been creating data-oriented algorithms for years. Recently, I decided to take a serious step towards Web Development and Software Programming as a career by enrolling in a bootcamp through edX and the University of Pennsylvania with a focus on the MERN full-stack of technologies.</p>
-            <p>I am 24 years old. I am currently located in Philadelphia and open for work. I have a passion for esports, since 2017 I have been running tournaments. My spare time is spent running, reading, and tending to my tiny windowsill garden.</p>
+            <p>My name is Ryan Sauter, and I am an aspiring Software Developer. Since my time at Purdue University, I have engaged with different Computer Science classes and the basics of coding. Starting with C and MATLAB, I have been creating data-oriented algorithms for years. Recently, I decided to take a serious step towards Web Development and Software Programming as a career by enrolling in a bootcamp through edX and the University of Pennsylvania with a focus on the MERN full-stack of technologies.</p>
+            <p>I am 24 years old. I am currently located in Philadelphia and working as a Web Developer for Rizen Metrics. I have a passion for esports, since 2017 I have been running tournaments. My spare time is spent running, reading, and tending to my tiny windowsill garden.</p>
         </>
     );
 }
